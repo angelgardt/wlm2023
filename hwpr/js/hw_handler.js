536acce47696be68f0d71c5868b526f83bc9ec3e
Anton Angelgardt <a.n.angelgardt@gmail.com>
2023-04-10 20:15:52 +0300

    final state no classes but autolabeled and autochecked with show hints after 3 incorrect submussion also hide autocheck without answer

--- hwpr/js/hw_handler.js
+++
@@ -1,8 +0,0 @@
-// import { Handler } from './handler.js';
-
-var hw_json_parced = JSON.parse(hw_json);
-
-const handler = new Handler(hw_json_parced, N_TASKS, ID);
-
-handler.showLevelLabs();
-handler.hideAutocheck();
