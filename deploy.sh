dd4491513e1472af8f0e8c2c488d6ec95dbbb7b6
Anton Angelgardt <a.n.angelgardt@gmail.com>
2023-05-03 22:19:19 +0300

    update deploy.sh, add cp redirect index.html

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,12 @@ function deployer {
 	GREEN='\033[0;32m'
 	GRAY='\033[0;37m'
 	NC='\033[0m' # No Color
-  
-  # pass inline arg to var
-  mode="${1:-update}"
-  # check deploy_dirs.txt exists
-  # abort function if not
+
+	# pass inline arg to var
+	mode="${1:-update}"
+
+	# check deploy_dirs.txt exists
+	# abort function if not
 	if [ ! -f deploy_dirs.txt ]; then
 		
 		echo -e "${RED}=====${NC}"
@@ -48,11 +49,11 @@ function deployer {
 			echo -e "${BLUE}docs directory created${NC}"
 			echo -e "-----"
 		fi
-
-    # check mode
-    if [ "$mode" != "update" ]; then
-      # if reset or update
-      rm -rf docs
+	
+		# check mode
+		if [ "$mode" != "update" ]; then
+			# if reset or render
+			rm -rf docs
 			echo -e "-----"
 			echo -e "${BLUE}docs directory removed${NC}"
 			echo -e "-----"
@@ -65,111 +66,125 @@ function deployer {
 			
 		# check mode
 		if [ "$mode" = "reset" ]; then
-		  
-		  # if reset
-		  ls -Ral docs
-		  echo -e "${GREEN}=====${NC}"
-		  echo -e "${GREEN}Reset completed${NC}"
-		  echo -e "Now your docs directory contains only empty README.md file"
-		  echo -e "${GREEN}=====${NC}"
-		  
+			# if reset
+			ls -Ral docs
+			echo -e "${GREEN}=====${NC}"
+			echo -e "${GREEN}Reset completed${NC}"
+			echo -e "Now your docs directory contains only empty README.md file"
+			echo -e "${GREEN}=====${NC}"
+				  
 		else
-		  
-		  # if update or render
-		  # check mode
-		  if [ "$mode" = "render" ]; then
-		    
-		    # if render
-		    # print dirs to render
-		    echo -e "-----"
-  	    echo -e "${BLUE}Directories to be rendered:${NC}"
-  	    for item in ${deploy_dirs[*]}
-  	    do
-  		    printf "${GRAY}    %s\n${NC}" $item
-  	    done
-  	    echo -e "-----"
-  	    
-		    for index in ${!deploy_dirs[*]}
-		    do
-		      # remove old _book dirs
-		      rm -rf `echo "${deploy_dirs[$index]}"/_book/`
-		      echo -e "-----"
-		      echo -e ${GRAY}${deploy_dirs[$index]}/_book/ ${BLUE}removed${NC}
-		      echo -e "-----"
-		      
-		      # render dirs
-		      {
-		        # try
-		        quarto render ${deploy_dirs[$index]} --to html
-		      } || {
-		        # catch
-		        echo -e "${RED}=====${NC}"
-		        echo -e "${RED}Deploy not completed${NC}"
-		        echo -e "${RED}File rendering error${NC}"
-		        echo -e "${RED}=====${NC}"
-    		    exit
-		      }
-		      
-		      # print dir render success
-		      echo -e "-----"
-		      echo -e "${GRAY}${deploy_dirs[$index]} ${BLUE}rendered${NC}"
-		      echo -e "-----"
-		    done
-		    
-		    # print render success
-		    echo -e "${GREEN}=====${NC}"
-		    echo -e "${GREEN}Render completed${NC}"
-		    echo -e "${GREEN}=====${NC}"
-		    
-		  fi
-  	  
-    	echo -e "-----"
-    	echo -e "${BLUE}Directories to be deployed:${NC}"
-    	for item in ${deploy_dirs[*]}
-    	do
-    		printf "${GRAY}    %s\n${NC}" $item
-    	done
-    	echo -e "-----"
+		
+			# if update or render
+			# check mode
+			if [ "$mode" = "render" ]; then
+			
+				# if render
+				# print dirs to render
+				echo -e "-----"
+				echo -e "${BLUE}Directories to be rendered:${NC}"
+				for item in ${deploy_dirs[*]}
+				do
+					printf "${GRAY}    %s\n${NC}" $item
+				done
+				echo -e "-----"
+			
+				for index in ${!deploy_dirs[*]}
+				do
+					# remove old _book dirs
+					rm -rf `echo "${deploy_dirs[$index]}"/_book/`
+					echo -e "-----"
+					echo -e ${GRAY}${deploy_dirs[$index]}/_book/ ${BLUE}removed${NC}
+					echo -e "-----"
+		     	 
+					# render dirs
+					{
+						# try
+						quarto render ${deploy_dirs[$index]} --to html
+					} || {
+						# catch
+						echo -e "${RED}=====${NC}"
+						echo -e "${RED}Deploy not completed${NC}"
+						echo -e "${RED}File rendering error${NC}"
+						echo -e "${RED}=====${NC}"
+						exit
+					}
+
+					# print dir render success
+					echo -e "-----"
+					echo -e "${GRAY}${deploy_dirs[$index]} ${BLUE}rendered${NC}"
+					echo -e "-----"
+				done
+				
+				# print render success
+				echo -e "${GREEN}=====${NC}"
+				echo -e "${GREEN}Render completed${NC}"
+				echo -e "${GREEN}=====${NC}"
+				
+			fi
+
+			{
+				#try
+				cp index.html docs/index.html
+			} || {
+				#catch
+				echo -e "-----"
+				echo -e ${RED}Deploy not completed${NC}
+				echo -e ${RED}redirecting index.html copying error${NC}
+				echo -e "-----"
+				exit
+			}
+
+			echo -e "-----"
+			echo -e ${GRAY}redirecting index.html${BLUE}copied${NC}
+			echo -e "-----"
+		
+			echo -e "-----"
+			echo -e "${BLUE}Directories to be deployed:${NC}"
+			for item in ${deploy_dirs[*]}
+			do
+				printf "${GRAY}    %s\n${NC}" $item
+			done
+			echo -e "-----"
     	
-    	for index in ${!deploy_dirs[*]}
-    	do
-    		if [ ! -d docs/"${deploy_names[$index]}" ]; then
-    			mkdir docs/"${deploy_names[$index]}"
-    			echo -e "-----"
-    			echo -e ${GRAY}docs/"${deploy_names[$index]}" ${BLUE}created${NC}
-    			echo -e "-----"
-    		fi
+			for index in ${!deploy_dirs[*]}
+			do
+				if [ ! -d docs/"${deploy_names[$index]}" ]; then
+					mkdir docs/"${deploy_names[$index]}"
+					echo -e "-----"
+					echo -e ${GRAY}docs/"${deploy_names[$index]}" ${BLUE}created${NC}
+					echo -e "-----"
+				fi
     		
-    		{ 
-    		  # try
-    		  cp -R `echo "${deploy_dirs[$index]}"/_book/*` `echo docs/"${deploy_names[$index]}"` 
-    		} || {
-    		  # catch
-    		  echo -e "${RED}=====${NC}"
-		      echo -e "${RED}Deploy not completed${NC}"
-		      echo -e "${RED}File copying error${NC}"
-		      echo -e "${RED}=====${NC}"
-    		  exit
-    		}
-    	  
-    		echo -e "-----"
-    		echo -e ${GRAY}"${deploy_dirs[$index]}" ${BLUE}copied${NC}
-    		echo -e "-----"
-    	done
-    
-    	# print list of docs files
-    	ls -Ral docs
-    	# print deploy success
-    	echo -e "${GREEN}=====${NC}"
-    	echo -e "${GREEN}Deploy completed${NC}"
-    	echo -e "${GREEN}=====${NC}"
-    
-    fi
-  
-  fi
-  
-  # reset text color
-  echo -e "${NC}"
+				{ 
+					# try
+					cp -R `echo "${deploy_dirs[$index]}"/_book/*` `echo docs/"${deploy_names[$index]}"` 
+				} || {
+					# catch
+					echo -e "${RED}=====${NC}"
+					echo -e "${RED}Deploy not completed${NC}"
+					echo -e "${RED}File copying error${NC}"
+					echo -e "${RED}=====${NC}"
+					exit
+				}
+				
+				echo -e "-----"
+				echo -e ${GRAY}"${deploy_dirs[$index]}" ${BLUE}copied${NC}
+				echo -e "-----"
+			done
+
+			# print list of docs files
+			ls -Ral docs
+			
+			# print deploy success
+			echo -e "${GREEN}=====${NC}"
+			echo -e "${GREEN}Deploy completed${NC}"
+			echo -e "${GREEN}=====${NC}"
+		fi
+	fi
+
+	# reset text color
+	echo -e "${NC}"
 }
 
 deployer $1
