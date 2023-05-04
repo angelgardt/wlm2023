e12bcc5d92cead3a1c6d4e6fb058296651228b5e
Anton Angelgardt <a.n.angelgardt@gmail.com>
2023-05-04 14:40:58 +0300

    update deploy.sh to manage with slides

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,9 @@ function deployer {
 	GRAY='\033[0;37m'
 	NC='\033[0m' # No Color
 
-	# pass inline arg to var
+	# pass inline args to vars
 	mode="${1:-update}"
+	slides="${2:-no}"
 
 	# check deploy_dirs.txt exists
 	# abort function if not
@@ -23,6 +24,28 @@ function deployer {
 		echo -e "Create a deploy_dirs.txt file with a list of direstories (each on a new line) that have to be deployed"
 		echo -e "Format of each line: ${GRAY}<original_dir_name>:<deployed_dir_name>${NC}"
 		echo -e "${RED}=====${NC}"
+		exit
+	
+	# check slides mode
+	# abort function if not valid
+	elif [ "$slides" != "slides" ] && [ "$slides" != "no" ]; then
+		
+		echo -e "${RED}=====${NC}"
+		echo -e "${RED}Deploy not completed${NC}"
+		echo -e "${RED}Unknown second inline argument${NC}"
+		echo -e "Valid options are ${GRAY}no${NC} (default) or ${GRAY}slides${NC}"
+		echo -e "${RED}=====${NC}"
+		exit
+	
+	# check slides mode and slides folder exists
+	# abort function if folder not exists
+	elif [ "$slides" == "slides" ] && [ ! -d slides ]; then
+			
+		echo -e "${RED}=====${NC}"
+		echo -e "${RED}Deploy not completed${NC}"
+		echo -e "${RED}slides mode is set but slides directory does not exists${NC}"
+		echo -e "${RED}=====${NC}"
+		exit
 	
 	# check mode
 	# abort function if not valid
@@ -30,12 +53,13 @@ function deployer {
 		
 		echo -e "${RED}=====${NC}"
 		echo -e "${RED}Deploy not completed${NC}"
-		echo -e "${RED}Uknown inline argument${NC}"
+		echo -e "${RED}Unknown first inline argument${NC}"
 		echo -e "Valid options are ${GRAY}update${NC} (default), ${GRAY}render${NC} or ${GRAY}reset${NC}"
 		echo -e "${RED}=====${NC}"
+		exit
 	
 	else
-	  
+		  
 		# read deploy_dirs.txt
 		deploy_dirs=( $(cut -d ":" -f1 deploy_dirs.txt) )
 		deploy_names=( $(cut -d ":" -f2 deploy_dirs.txt) )
@@ -116,6 +140,39 @@ function deployer {
 					echo -e "-----"
 				done
 				
+				# check slides mode
+				# if slides save qmd paths for rendering
+				if ["$slides" == "slides" ]; then
+
+					echo -e "${BLUE}Following slides found${NC}"
+					
+					i=0
+					while read line
+					do
+						slides_qmds[ $i ]="$line"
+						printf "${GRAY}    %s\n${NC}" $line
+						(( i++ ))
+					done < <(find slides -name *.qmd)
+					
+					for slide in ${slides_qmds[*]}
+					do
+						
+						# render slides
+						{
+							# try
+							quarto render $slide
+						} || {
+							#catch
+							echo -e "${RED}=====${NC}"
+							echo -e "${RED}Deploy not completed${NC}"
+							echo -e "${RED}Slides rendering error${NC}"
+							echo -e "${RED}=====${NC}"
+							exit
+						}
+					done
+				
+				fi
+				
 				# print render success
 				echo -e "${GREEN}=====${NC}"
 				echo -e "${GREEN}Render completed${NC}"
@@ -146,7 +203,7 @@ function deployer {
 				printf "${GRAY}    %s\n${NC}" $item
 			done
 			echo -e "-----"
-    	
+    			
 			for index in ${!deploy_dirs[*]}
 			do
 				if [ ! -d docs/"${deploy_names[$index]}" ]; then
@@ -172,7 +229,74 @@ function deployer {
 				echo -e ${GRAY}"${deploy_dirs[$index]}" ${BLUE}copied${NC}
 				echo -e "-----"
 			done
-
+			
+			# check slides mode
+			# run copying files if slides
+			if [ "$slides" == "slides" ]; then
+				
+				if [ ! -d docs/slides ]; then
+					mkdir docs/slides
+					echo -e "-----"
+					echo -e ${GRAY} docs/slides ${BLUE}created${NC}
+					echo -e "-----"
+				fi
+				
+				
+				echo -e "${BLUE}Following slides found${NC}"
+				
+				i=0
+				while read line
+				do
+					slides_htmls[ $i ]="$line"
+					printf "${GRAY}    %s\n${NC}" $line
+					(( i++ ))
+				done < <(find slides -name *.html)
+					
+				for slide in ${slides_htmls[*]}
+				do
+						
+					# copy slides
+					{
+						# try
+						cp $slide `echo docs/"$slide"`
+					} || {
+						#catch
+						echo -e "${RED}=====${NC}"
+						echo -e "${RED}Deploy not completed${NC}"
+						echo -e "${RED}Slides copying error${NC}"
+						echo -e "${RED}=====${NC}"
+						exit
+					}
+				done
+				
+				echo -e "${BLUE}Following slides files found${NC}"
+				
+				i=0
+				while read line
+				do
+					slides_files[ $i ]="$line"
+					printf "${GRAY}    %s\n${NC}" $line
+					(( i++ ))
+				done < <(find slides -name *_files)
+				
+				for files in ${slides_files[*]}
+				do
+					
+					# copy slides files
+					{
+						# try
+						cp -R `echo "$files"/*` `echo docs/"$files"`
+					} || {
+						# catch
+						echo -e "${RED}=====${NC}"
+						echo -e "${RED}Deploy not completed${NC}"
+						echo -e "${RED}Slides files copying error${NC}"
+						echo -e "${RED}=====${NC}"
+						exit
+					}
+				done
+			fi
+			
 			# print list of docs files
 			ls -Ral docs
 			
@@ -187,4 +311,4 @@ function deployer {
 	echo -e "${NC}"
 }
 
-deployer $1
+deployer $1 $2
