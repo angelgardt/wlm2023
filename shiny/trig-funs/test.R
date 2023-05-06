cd1f4be3e5e4da93b930f6ce5d82ba492bb9e972
Anton Angelgardt <a.n.angelgardt@gmail.com>
2023-05-06 18:21:57 +0300

    create first version of trig-funs

---
+++ shiny/trig-funs/test.R
@@ -0,0 +1,72 @@
+library(ggplot2)
+library(tibble)
+library(ggforce)
+
+theme_set(theme_bw())
+
+# axes limits
+xlim = 1.5
+ylim = 1.5
+step = .5
+
+# funcs
+funcs = c("sin", "cos", "sec", "csc", "tan", "cot")
+
+# set colors
+colors = list(
+  sin = "red",
+  cos = "blue",
+  sec = "magenta",
+  csc = "cyan",
+  tan = "gold",
+  cot = "orange"
+)
+
+
+# calculate trig funs
+input = 300
+phi = NISTunits::NISTdegTOradian(input)
+cosphi = cos(phi)
+sinphi = sin(phi)
+secphi = 1 / cos(phi)
+cscphi = 1 / sin(phi)
+tanphi = tan(phi)
+cotphi = 1 / tan(phi)
+
+# set coords
+coords = tibble(func = factor(rep(funcs, each = 2),
+                              ordered = TRUE,
+                              levels = funcs),
+                point = rep(c("start", "end"), times = 6),
+       x = c(cosphi, cosphi, 0, cosphi, 0, 1, 0, 0, 1, 1, 0, cosphi),
+       y = c(0, sinphi, 0, 0, 0, tanphi, 0, cscphi, 0, tanphi, cscphi, sinphi))
+
+# plot
+ggplot() +
+  # axes
+  geom_hline(yintercept = 0, color = "gray") +
+  geom_vline(xintercept = 0, color = "gray") +
+  # circle
+  geom_circle(data = NULL,
+              aes(x0 = 0, y0 = 0, r = 1)) +
+  # raduis vector
+  geom_line(data = NULL,
+            aes(x = c(0, cosphi),
+                y = c(0, sinphi))) +
+  # sin
+  geom_line(data = coords,
+            aes(x = x,
+                y = y,
+                color = func)) +
+  # dot
+  geom_point(data = NULL,
+             aes(x = cosphi,
+                 y = sinphi)) +
+  scale_x_continuous(breaks = seq(-xlim, xlim, by = step)) +
+  scale_y_continuous(breaks = seq(-ylim, ylim, by = step)) +
+  labs(x = NULL, y = NULL) +
+  coord_fixed(xlim = c(-xlim, xlim),
+                  ylim = c(-ylim, ylim)) +
+  theme(legend.position = "bottom") +
+  scale_color_manual(values = unlist(colors))
+
