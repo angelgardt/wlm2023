e90a8efa4b08b23ecd717175ed0b40bb9c0ded2b
Anton Angelgardt <a.n.angelgardt@gmail.com>
2023-11-20 20:52:38 +0300

    add data for j-form1

---
+++ proj/solutions/j_form1.R
@@ -0,0 +1,100 @@
+# J // Form 1
+
+library(tidyverse)
+theme_set(theme_bw())
+
+mr_preproc <- function(d) {
+  
+  require(tidyverse)
+  
+  d |> select(
+    # select columns we need
+    correctAns,
+    base_pic,
+    rotated_pic,
+    resp_MR_easy.keys,
+    resp_MR_easy.corr,
+    resp_MR_easy.rt
+  ) |>
+    drop_na() |> # remove technical NAs (recording artefacts, not missing data)
+    mutate(task = "MR",
+           # add task name (mental rotation)
+           level = "easy",
+           # add difficulty level
+           trial = 1:16) |> # number trials
+    rename(
+      # rename columns for handy usage
+      "key" = resp_MR_easy.keys,
+      "is_correct" = resp_MR_easy.corr,
+      "rt" = resp_MR_easy.rt
+    ) -> MR_easy # ready to use
+  
+  
+  d |> select(
+    # select columns we need
+    correctAns,
+    base_pic,
+    rotated_pic,
+    resp_MR_medium.keys,
+    resp_MR_medium.corr,
+    resp_MR_medium.rt
+  ) |>
+    drop_na() |> # remove technical NAs (recording artefacts, not missing data)
+    mutate(task = "MR",
+           # add task name (mental rotation)
+           level = "medium",
+           # add difficulty level
+           trial = 1:16) |>  # number trials
+    rename(
+      # rename columns for handy usage
+      "key" = resp_MR_medium.keys,
+      "is_correct" = resp_MR_medium.corr,
+      "rt" = resp_MR_medium.rt
+    ) -> MR_medium # ready to use
+  
+  
+  
+  d |> select(
+    # select columns we need
+    correctAns,
+    base_pic,
+    rotated_pic,
+    resp_MR_hard.keys,
+    resp_MR_hard.corr,
+    resp_MR_hard.rt
+  ) |>
+    drop_na() |> # remove technical NAs (recording artefacts, not missing data)
+    mutate(task = "MR",
+           # add task name (mental rotation)
+           level = "hard",
+           # add difficulty level
+           trial = 1:16) |> # number trials
+    rename(
+      # rename columns for handy usage
+      "key" = resp_MR_hard.keys,
+      "is_correct" = resp_MR_hard.corr,
+      "rt" = resp_MR_hard.rt
+    ) -> MR_hard # ready to use
+  
+  # bind all conditions of mental rotation task to one tibble
+  
+  bind_rows(MR_easy, MR_medium, MR_hard) -> MR
+  
+  return(MR)
+  
+}
+
+files <- paste0("../data/pool34/", dir("../data/pool34/"))
+
+MR_data <- tibble()
+
+for (i in 1:length(files)) {
+  
+  print(files[i])
+  
+  d <- read_csv(files[i], show_col_types = FALSE)
+  
+  MR_data |> bind_rows(mr_preproc(d) |> mutate(file = files[i])) -> MR_data
+  
+}
+
