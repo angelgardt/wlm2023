9aaacd2373cd76d5e7d047112129456c98a67155
Anton Angelgardt <a.n.angelgardt@gmail.com>
2024-04-02 17:04:47 +0300

    hw16 done

--- hw/solutions/hw16.R
+++ hw/solutions/hw16.R
@@ -26,6 +26,7 @@ OPN =~ OPN1 + OPN2 + OPN3 + OPN4 + OPN5 + OPN6 + OPN7 + OPN8 + OPN9 + OPN10
 "
 
 model1 <- cfa(mdl1, bffm)
+summary(model1)
 
 
 # 4
@@ -56,6 +57,7 @@ PER =~ AGR + CSN + EST + EXT + OPN
 "
 
 model2 <- cfa(mdl2, bffm)
+summary(model2)
 
 
 # 7
@@ -66,28 +68,46 @@ fitmeasures(model2, c("CFI", "TLI", "SRMR", "RMSEA"))
 # 8
 mis2 <- modificationindices(model2) %>% arrange(desc(mi))
 
-## corrs between item from same scale
-mis2 %>% 
-  filter(str_sub(lhs, 1, 3) == str_sub(rhs, 1, 3)) %>% 
-  ggplot(aes(rhs, mi)) +
-  geom_point() +
-  facet_wrap(~ lhs, scales = "free")
+choose(5, 2)
 
-## corrs between item from different scale
 mis2 %>% 
-  filter(str_sub(lhs, 1, 3) != str_sub(rhs, 1, 3)) %>% 
-  filter(str_detect(lhs, "^[[:upper:]]{3}$")) %>% 
-  ggplot(aes(rhs, mi)) +
-  geom_point() +
-  theme(axis.text.x = element_text(angle = 90)) +
-  facet_wrap(~ lhs, scales = "free")
-
+  filter(lhs != rhs &
+           !str_detect(lhs, "\\d$") &
+           !str_detect(rhs, "\\d$")) # %>% select(lhs, op, rhs)
 
 
 # 9
+mdl3 <- "
+AGR =~ AGR1 + AGR2 + AGR3 + AGR4 + AGR5 + AGR6 + AGR7 + AGR8 + AGR9 + AGR10
+CSN =~ CSN1 + CSN2 + CSN3 + CSN4 + CSN5 + CSN6 + CSN7 + CSN8 + CSN9 + CSN10
+EST =~ EST1 + EST2 + EST3 + EST4 + EST5 + EST6 + EST7 + EST8 + EST9 + EST10
+EXT =~ EXT1 + EXT2 + EXT3 + EXT4 + EXT5 + EXT6 + EXT7 + EXT8 + EXT9 + EXT10
+OPN =~ OPN1 + OPN2 + OPN3 + OPN4 + OPN5 + OPN6 + OPN7 + OPN8 + OPN9 + OPN10
+PER =~ AGR + CSN + EST + EXT + OPN
+CSN ~~ EST
+AGR ~~ EST
+CSN ~~ EXT
+AGR ~~ EXT
+EST ~~ EXT
+AGR ~~ CSN
+AGR ~~ OPN
+EXT ~~ OPN
+EST ~~ OPN
+CSN ~~ OPN
+"
+
+model3 <- cfa(mdl3, bffm)
+
+fitmeasures(model1, c("CFI", "TLI", "SRMR", "RMSEA"))
+fitmeasures(model2, c("CFI", "TLI", "SRMR", "RMSEA"))
+fitmeasures(model3, c("CFI", "TLI", "SRMR", "RMSEA"))
 
 
 # 10
+
+anova(model1, model2)
+anova(model1, model3)
+anova(model2, model3)
 
 
 
